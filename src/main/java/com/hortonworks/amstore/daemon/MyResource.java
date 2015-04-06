398e73eee0b7359ff300fbb8d46896a3c1f35b2c
root <root@sandbox.local>
2015-04-06 13:56:55 -0700

    added bash scripts

--- src/main/java/com/hortonworks/amstore/daemon/MyResource.java
+++ src/main/java/com/hortonworks/amstore/daemon/MyResource.java
@@ -1,5 +1,9 @@
 package com.hortonworks.amstore.daemon;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -19,7 +23,16 @@ public class MyResource {
      */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    public String getIt() throws IOException {
+
+		ProcessBuilder p = new ProcessBuilder();
+		p.command("nohup", "/etc/init.d/ambari-server", "restart");
+		p.redirectOutput(Redirect.appendTo(new File(
+				"/var/log/amstore-daemon.log")));
+		p.redirectErrorStream(true);
+		p.start();
+
+		// Runtime.getRuntime().exec("/etc/init.d/ambari-server restart");
+		return "Ambari restart initiated. Please wait.";
     }
 }
